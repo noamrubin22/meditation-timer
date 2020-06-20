21c80c7b70030476be5cb9e8ce5590c5e61ff84b
Noam <noamru22@gmail.com>
2020-06-20 20:13:04 +0200

    motor and display work, only a few minor things to do

--- meditation-timer.ino
+++ meditation-timer.ino
@@ -1,81 +1,114 @@
 #include <RotaryEncoder.h>
 #include "SevSeg.h"
+#include <Servo.h>
 //#include <Serial.h>
-#include <SimpleTimer.h>
+//#include <SimpleTimer.h>
 
+// initalizing the rotary encoder switch 
 int sw_pin = A5;
-int num;
 
+// initalizing motor
+Servo myservo; 
+
+// define rotary input pins
 RotaryEncoder encoder(A4, A3);
 
-SevSeg sevseg; //Initiate a seven segment controller object
-// initializing the timer object 
-//SimpleTimer timer;
+// initiate a seven segment controller object
+SevSeg sevseg; 
+//// initializing the timer object 
+////SimpleTimer timer;
 
 void setup() {
-byte numDigits = 4;
-byte digitPins[] = {2, 3, 4, 5};
-byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
-sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
-sevseg.setBrightness(90);
+  // setting up 7segments display 
+  byte numDigits = 4;
+  byte digitPins[] = {A2, 3, 4, 5};
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  sevseg.setBrightness(90);
+  
+  // use pull-up as a internal resistor. 
+  // if you want to read a button you should always use a pullup 
+  // we are using this to prevent "floating"
+  // https://learn.sparkfun.com/tutorials/pull-up-resistors/all
+  // when the button is pushed, the current is LOW, otherwise HIGH
+  pinMode(sw_pin, INPUT_PULLUP);
 
-// use pull-up as a internal resistor. 
-// if you want to read a button you should always use a pullup
-pinMode(sw_pin, INPUT_PULLUP);
-
-Serial.begin(57600);
+  // define motor pin
+  myservo.attach(2);
+  myservo.write(0);
+  
+  Serial.begin(57600);
 }
-
-bool flag = 0;
-
+// the different states we have
 enum State {
   INIT,
   SELECT_TIME
 };
 
+// set variables
 int countdown = 0;
-int newPos = 0;
-int previousMillis=0;
-int interval = 300;
+unsigned long time_stamp = 0;
+bool do_gong = false;
+bool positioning_time = false;
+static State state = SELECT_TIME;
 
 void loop()
 {
   static bool pressed = false;
-  static State state = INIT;
   
-  if(!digitalRead(sw_pin) )
+  // Change the state between INIT and SELECT_TIME, each button press
+  // if button is pressed set to true
+  if(!digitalRead(sw_pin)) {
     pressed = true;
+  }
+  // when button is not pressed/released, set to false
   else if (digitalRead(sw_pin) && pressed) {
     pressed = false;
+    // go to the next state
     state = (State)((state + 1) % 2);
   }
-    
- if (state == SELECT_TIME){
-  encoder.tick();
-  newPos = encoder.getPosition();
-  sevseg.setNumber(newPos, 2);
-  sevseg.refreshDisplay();
-  countdown = newPos;
-  flag = true;
- } else {
-  if(flag){
-    display_num(countdown);
-  } else {
+
+ // if we are in the configuration state, set countdown with chosen number
+  if (state == SELECT_TIME){
+    encoder.tick();
+    countdown = encoder.getPosition();
     sevseg.setNumber(countdown, 2);
-    sevseg.refreshDisplay(); // Must run repeatedly
-  }
+    sevseg.refreshDisplay();
+    // start gong when time is set and add positioning time
+    do_gong = true;
+    positioning_time = true;
+  } else {
+  // if we are in the INIT state
+      if (positioning_time){
+        delay(5000);
+        positioning_time = false;
+      }
+      // start countdown
+      if (do_gong) {
+       // do the gong, reset the counter and set countingdown flag to counting.
+       myservo.write(135);
+       delay(200);
+       myservo.write(0);
+       do_gong = false;
+       time_stamp = millis();
+      } else {
+        display_countdown(countdown, time_stamp);
+      }
  }
-    //display_num(newPos);
-    //sevseg.blank();
 }
 
-void display_num(int num){
+void display_countdown(int num, unsigned long time_stamp){
 
   int displayed;
   int counter = (num*60);
-  int seconds = counter - (millis() / 1000);
-  
-  if (seconds < 60) {
+  int seconds = counter - ((millis() - time_stamp) / 1000);
+
+  // display minutes and seconds, without negatives
+  if (seconds < 0) {
+    seconds = 0;
+    do_gong = true;
+  }
+  else if (seconds < 60) {
     displayed = seconds;
   } else {
     int minutes;
@@ -88,3 +121,7 @@ void display_num(int num){
   sevseg.setNumber(displayed, 2); 
   sevseg.refreshDisplay(); // Must run repeatedly
 }
+
+// to do for next time: 
+// remove negatives in SELECTION_TIME mode. 
+// add a delay without messing with the display for positioning_time
